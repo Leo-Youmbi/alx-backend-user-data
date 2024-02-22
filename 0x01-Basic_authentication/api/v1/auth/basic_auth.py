cb0708c8f769af03165d07af0b053f1a11a59fe9
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-02-22 07:22:02 +0100

    simple api task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 
 
+import base64
+import binascii
 from api.v1.auth import Auth
 
 
@@ -20,4 +22,22 @@ class BasicAuth(Auth):
            and isinstance(auth_header, str) \
            and auth_header.startswith("Basic "):
             return auth_header.split(" ")[1]
+        return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """Decode base64 authorization header
+        """
+        auth_header = base64_authorization_header
+        try:
+            if auth_header is not None and isinstance(auth_header, str):
+                decoded_header = base64.b64decode(
+                    auth_header,
+                    validate=True
+                    )
+                return decoded_header.decode('utf-8')
+        except binascii.Error:
+            return None
         return None
