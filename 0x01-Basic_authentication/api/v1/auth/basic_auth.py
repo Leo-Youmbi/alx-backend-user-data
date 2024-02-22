f292b167030bb5755719832af3622da00b44a22b
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-02-22 06:58:04 +0100

    simple api task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,15 @@ from api.v1.auth import Auth
 class BasicAuth(Auth):
     """Basic Authentication class
     """
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
+        """Extract base64 authorization header
+        """
+        auth_header = authorization_header
+        if auth_header is not None \
+           and isinstance(auth_header, str) \
+           and auth_header.startswith("Basic "):
+            return auth_header.split(" ")[1]
+        return None
