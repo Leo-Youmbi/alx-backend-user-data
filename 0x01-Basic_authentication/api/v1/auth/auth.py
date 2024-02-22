5deabe192c831d2151d022228ba7604cde9c9a91
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-02-22 05:29:04 +0100

    simple api task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if url requires authentication
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Returns authorization header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns current user
+        """
+        return None
