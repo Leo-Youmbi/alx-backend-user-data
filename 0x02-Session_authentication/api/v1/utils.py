83efc4b14865383099f587dc84cac37ea517e786
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-02-23 04:06:21 +0100

    session_auth task 3

--- 0x02-Session_authentication/api/v1/utils.py
+++ 0x02-Session_authentication/api/v1/utils.py
@@ -4,10 +4,11 @@
 
 
 from typing import Any
+from uuid import UUID
 
 
 def isNotNoneAndIsAString(variable: Any) -> bool:
     """Check if variable is not None and is a string
     """
     return variable is not None \
-        and isinstance(variable, str)
+        and isinstance(variable, (str, UUID))
