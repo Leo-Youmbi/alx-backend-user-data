bd13d415f1c3b0b94a013ec933b0f426fcc90587
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-02-23 03:38:51 +0100

    session_auth task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,9 +11,18 @@ from api.v1.utils import isNotNoneAndIsAString
 class SessionAuth(Auth):
     """Session Authentication class
     """
+
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """Create a new session
+
+        Args:
+            user_id (str, optional): The id of a user. Defaults to None.
+
+        Returns:
+            str: the id of the newly created id
+        """
         if isNotNoneAndIsAString(user_id):
             session_id = uuid.uuid4()
             type(self).user_id_by_session_id.__setitem__(session_id, user_id)
